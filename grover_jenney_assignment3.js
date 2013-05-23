0f9937330f10414b1354bfa87572ab22890646b8
Jenney Grover <jenneygrover@fullsail.edu>
2013-05-22 22:56:26 -0400

    added code
    boolean conditional

--- grover_jenney_assignment3.js
+++ grover_jenney_assignment3.js
@@ -20,19 +20,30 @@ function timer()
 }
 
 
-var i //the counter variable for function use
+var i = 0 //the counter variable for function use
 var exerciser = "Jaesyn";
-var pyramidChallenge = ["walking", "kickboxing", "Tai Chi", "DDP Yoga", "jogging", "TTapp", "pilates"];
+var pyramidChallenge = [" walking ", " kickboxing ", " Tai Chi ", " DDP Yoga ", " jogging ", " TTapp ", " pilates "];
 var workoutDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var durationWorkout = [5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5];
 var workoutChoices = pyramidChallenge.length; 
-var timeToWorkout = 60; 
+var freeTime = 60; 
+var workoutTime = 25; 
 var gonnaWorkout = true; 
 var gonnaSkip = false; 
 var quoteLife10 = "Life is 10% what happens to you and 90% how handle it.";
 var quoteNever = "\"Never give in and never give up.\" ~ Hubert H. Humphrey";
 var someday = "Someday is not a day of the week"; 
-var workoutMinutes=(timeToWorkout<60)?"Do the quick workout!":"You got time to get the full burn!";
+var workoutMinutes=(freeTime<60)?"Do the quick workout!":"You got time to get the full burn!";
 
+//procedure
+console.log("Welcome " + exerciser + " to your workout! Which will you chose today? " + pyramidChallenge);
 
+//boolean conditional 
+if (freeTime>workoutTime) {
+	console.log("You have time to workout! Your workout is: " + pyramidChallenge[i]);
+	i++
+	//freeTime = freeTime - workoutTime; 
+	} else {
+	console.log("You have time for a quick walk around the neighborhood."); 	
+}
 	
